aebd4154b2ee54b4c1c89719643e2ccbf06995b0
Meeran <mee.sidd.1919@gmail.com>
2025-07-24 16:21:50 +0530

    Created seeds file for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_24_071952) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_24_104410) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -23,6 +23,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_24_071952) do
     t.string "subtitle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
   end
 
   create_table "skills", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,32 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+10.times do |blog|
+  Blog.create!(
+     title: "My Blog Post #{blog}",
+     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  )
+end
+
+
+5.times do |skill|
+  Skill.create!(
+      title: "Rails #{skill}",
+      percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+     title: "Portfolio title: #{portfolio_item}",
+     subtitle: "My great service",
+     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      main_image: "https://place-hold.it/600x400",
+      thumb_image: "https://place-hold.it/350x200"
+  )
+end
+
+puts "9 portfolio items created"
+
+
+# rails new app_name -d postgresql
