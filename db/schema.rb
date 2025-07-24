717d84d7ffccb76a68f37a3e3078357b5a6f9358
Meeran <mee.sidd.1919@gmail.com>
2025-07-24 12:59:05 +0530

    Added portfolio items via Resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_24_060739) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_24_071952) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
