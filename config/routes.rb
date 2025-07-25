7992c54977fb2e37491a33b43b0b55f702cb9161
Meeran <mee.sidd.1919@gmail.com>
2025-07-25 12:50:53 +0530

    Updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  # Now  i want that except for show action for rest of all portfolios (plural) actions is accepted but for show action only one portfolio (singular) is accepted for this ->
+
+  resources :portfolios, except: [ :show ]
+
+  get "portfolio/:id", to: "portfolios#show", as: :'portfolio_show'
 
 
   # get "pages/about"
   # get "pages/contact"
 
-  get "about", to: "pages#about"
+  get "about-me", to: "pages#about"
   get "contact", to: "pages#contact"
 
   resources :blogs
