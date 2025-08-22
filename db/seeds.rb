8198a70a011a843ebd8542cfac5a172470323c6c
Meeran <mee.sidd.1919@gmail.com>
2025-08-22 21:37:43 +0530

    Implemented partials for navigation, footer,  and masthead for the portfolio layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,8 +30,8 @@ puts "5 skills created"
      title: "Portfolio title: #{portfolio_item}",
      subtitle: "Ruby on Rails",
      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      main_image: "https://placehold.co/300x200",
-      thumb_image: "https://placehold.co/300x150"
+      main_image: "https://placehold.co/400x150",
+      thumb_image: "https://placehold.co/400x150"
     # thumb_image: "https://static.vecteezy.com/system/resources/thumbnails/014/960/335/small/red-yellow-solid-color-wallpaper-image-for-free-background-download-free-photo.jpg"
   )
 end
@@ -43,8 +43,8 @@ puts "9 portfolio items created"
      title: "Portfolio title: #{portfolio_item}",
      subtitle: "Angular",
      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      main_image: "https://placehold.co/300x200",
-      thumb_image: "https://placehold.co/300x150"
+      main_image: "https://placehold.co/400x150",
+      thumb_image: "https://placehold.co/400x150"
     # thumb_image: "https://static.vecteezy.com/system/resources/thumbnails/014/960/335/small/red-yellow-solid-color-wallpaper-image-for-free-background-download-free-photo.jpg"
   )
 end
